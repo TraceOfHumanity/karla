d99518de5eba52764b5a64c914efbb2f8e2d7970
Alexander_Ovchar <azalexxx02@gmail.com>
2025-03-08 20:17:30 +0200

    Implement basic heatmap rendering and remove StrictMode

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,31 @@ function App({ data }: AppProps) {
     if (heatMapRef.current) {
       const heatMap = d3.select(heatMapRef.current);
       console.log(heatMap);
+
+      const dimensions = {
+        window: 600,
+        height: 150,
+      }
+
+      const boxSize = 30;
+
+      const svg = heatMap.append("svg")
+        .attr("width", dimensions.window)
+        .attr("height", dimensions.height)
+        // .attr("viewBox", `0 0 ${dimensions.window} ${dimensions.height}`)
+        // .attr("preserveAspectRatio", "xMidYMid meet")
+
+      svg.append("g")
+        .attr("transform", `translate(2, 2)`)
+        .attr("stroke", "black")
+        .attr("fill", "#ddd")
+        .selectAll("rect")
+        .data(data)
+        .join("rect")
+        .attr("width", boxSize - 3)
+        .attr("height", boxSize - 3)
+        .attr("x", (d, index) => boxSize * (index % 10))
+        .attr("y", (d, index) => boxSize * Math.floor(index / 10))
     }
   }
 
