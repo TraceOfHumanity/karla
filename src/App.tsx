a647862a89899e506aa26e84f3814e441578eb79
Alexander_Ovchar <azalexxx02@gmail.com>
2025-03-08 19:59:29 +0200

    Set up initial D3 heatmap structure with sample data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-function App() {
+import * as d3 from "d3";
+import { useEffect, useRef } from "react";
 
+interface AppProps {
+  data: number[];
+}
+
+function App({ data }: AppProps) {
+  const heatMapRef = useRef<HTMLDivElement>(null);
+
+  const drawHeatMap = async () => {
+    console.log(data);
+    if (heatMapRef.current) {
+      const heatMap = d3.select(heatMapRef.current);
+      console.log(heatMap);
+    }
+  }
+
+  useEffect(() => {
+    drawHeatMap();
+  }, [])
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <div ref={heatMapRef}></div>
   )
 }
 
